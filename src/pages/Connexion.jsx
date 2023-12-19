1cc0f52d4cef1b06810afb51a864765a13ff58ad
kessoba <mariamabahdiallo1@gmail.com>
2023-12-19 13:53:16 +0000

    nouveauté

--- src/pages/Connexion.jsx
+++ src/pages/Connexion.jsx
@@ -5,32 +5,54 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import affiche from '../assets/images/affiche.jpg'
 export default function Connexion() {
   return (
-    <div className='container'>Connexion
-      <div className='row d-flex '>
-        <div className='col-md-6 '>
-          <div><img src={affiche} alt="" className='img-fluid' /></div>
-        </div>
-      <div className='col-md-6 justify-content-center'>
+    
+  <div class="container-fluid contain-connect  ">
+    <div class="row d-flex justify-content-center align-items-center ">
+          <div class="row g-0 justify-content-arround">
+            <div class=" height-img  col-lg-5 d-none d-md-block ">
+              <img src={affiche}
+                alt="login form" className="img-fluid rounded-6" />
+            </div>
+            <div class="col-lg-7 height-form d-flex justify-content-center">
+              <div class="card-body   p-lg-5 text-black">
+              <div class="d-flex justtify-content-center   bouton-switch  ">
+              <button type="button" class="btn btn-outline-success btn-rounded tir  ">Login</button>
+              <button type="button" class="btn btn-outline-success btn-rounded tir">Register</button> 
+              </div>
+                <form >
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat recusandae</p>
+                  <div class="form-outline mb-4">
+                  <label class="form-label" for="form2Example17">Addresse Email</label>
+                    <input type="email" id="form2Example17" class="form-control  rounded-3 form-control-lg" placeholder='Email' />
+                    
+                  </div>
+
+                  <div class="form-outline mb-4">
+                  <label class="form-label" for="form2Example27">Mot de Pass</label>
+                    <input type="password" id="form2Example27" class="form-control form-control-lg" placeholder='Mot De Pass' />
+                  
+                  </div>
+
+                  <div class="pt-1 mb-4">
+                    <button class="btn btn-outline-success btn-lg btn-block tir" type="button">Login</button>
+                  </div>
+
+                  <Link
+                      to="/Modal"
+                      onClick={handleShowModal}
+                      className="text-decoration-none"
+                    >
+                      <p className="m-0 p-0">Mot de passe oublié?</p>
+                    </Link>
+                </form>
+
+              </div>
+            </div>
+          </div>
+        
       
-    <form className='justify-content-center'>
-    <div className=''>les deux bouttons</div>
-    <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-      </div>
-      </div>
     </div>
+  </div>
+  
   )
 }
