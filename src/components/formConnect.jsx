4b321da92a4bc73ec22917e28e3a25c5cb410ba4
MeedBechir <medbechirathie@gmail.com>
2023-12-19 17:03:39 +0000

    Connexion et Mot de passe Oublié géré

--- src/components/formConnect.jsx
+++ src/components/formConnect.jsx
@@ -1,60 +1,130 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import {useState} from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { CiMail } from "react-icons/ci";
 import { RiLockPasswordFill } from "react-icons/ri";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import {
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../config/firebase";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 export default function FormConnect() {
-    // state pour le modal mot de pass oublier
-   const [showModal, setShowModal] = useState(false);
+  // Les hooks
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-   const handleShowModal = () => setShowModal(true);
-   // const handleHideModal = () => setOnHide(false);
-   // render login
+
+  // UseNavigate pour les redirections
+  const navigate = useNavigate();
+
+  // les changements dans les champs
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  // Fonction pour la connexion / login
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+
+      setEmail("");
+      setPassword("");
+
+      navigate("/dashboardEtudiant");
+    } catch (error) {
+      toast.error("Échec de la connexion. Veuillez vérifier vos informations.");
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div> <div class="card-body justify-content-center  p-lg-5 text-black">
-    <div class="d-flex  mb-5 justtify-content-center   bouton-switch  ">
-    <button type="button" class="btn  tir text-white ">Login</button>
-    <Link to="/" >
-    <button type="button" class="btn  tir text-white ">Register</button>
-    </Link> 
-    </div>
-      <form >
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat recusandae</p>
-        <div class=" tire mb-4">
-        <div class="input-group  flex-nowrap">
-      <span class="input-group-text" id="addon-wrapping"><CiMail />
-      </span>
-      <input type="email" class="form-control " placeholder="Email" aria-label="Username" aria-describedby="addon-wrapping"/>
-        </div> 
+    <div>
+      <ToastContainer />
+      <div class="card-body justify-content-center  p-lg-5 text-black">
+        <div class="d-flex  mb-5 justtify-content-center   bouton-switch  ">
+          <button type="button" class="btn  tir text-white ">
+            Login
+          </button>
+          <Link to="/">
+            <button type="button" class="btn  tir text-white ">
+              Register
+            </button>
+          </Link>
         </div>
+        <form onSubmit={handleLogin}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            recusandae
+          </p>
+          <div class=" tire mb-4">
+            <div class="input-group  flex-nowrap">
+              <span class="input-group-text" id="addon-wrapping">
+                <CiMail />
+              </span>
+              <input
+                type="email"
+                class="form-control"
+                onChange={handleEmailChange}
+                placeholder="Email"
+                aria-label="Username"
+                aria-describedby="addon-wrapping"
+              />
+            </div>
+          </div>
 
-        <div class=" tire ">
-        <div class="input-group mb-3  flex-nowrap">
-        <span class="input-group-text" id="addon-wrapping"><RiLockPasswordFill />
-          </span>
-        <input type="password" class="form-control " placeholder="Password" aria-label="Username" aria-describedby="addon-wrapping"/>
-        </div>
-        
-        </div>
-        <Link
+          <div class=" tire ">
+            <div class="input-group mb-3  flex-nowrap">
+              <span class="input-group-text" id="addon-wrapping">
+                <RiLockPasswordFill />
+              </span>
+              <input
+                type="password"
+                class="form-control "
+                onChange={handlePasswordChange}
+                placeholder="Password"
+                aria-label="Username"
+                aria-describedby="addon-wrapping"
+              />
+            </div>
+          </div>
+          <Link
             to="/Modal"
-            onClick={handleShowModal}
             className="text-decoration-none"
           >
             <p className="m-0 p-0 text-start oubli">Mot de passe oublié?</p>
           </Link>
 
-        <div class="pt-1 mt-4 text-end ">
-          <button class="btn  btn-lg btn-block text-white  log" type="button">Login</button>
-        </div>
-
-       
-      </form>
-
-    </div> </div>
-  )
+          <div class="pt-1 mt-4 text-end ">
+            <button
+              type="submit"
+              className="btn btn-primary btn-md"
+              onClick={handleLogin}
+            >
+              {/* Loader */}
+              {loading ? (
+                <div className="spinner-border" role="status">
+                  <span className="visually-hidden">Chargement...</span>
+                </div>
+              ) : (
+                "Se connecter"
+              )}
+            </button>
+          </div>
+        </form>
+      </div>{" "}
+    </div>
+  );
 }
--- src/pages/OubliMoPass.jsx
+++ src/pages/OubliMoPass.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../config/firebase';
 
 export default function OubliMoPass() {
-
+  const [forgotPasswordEmail, setForgotPasswordEmail] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(true);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const emailVal = e.target.email.value;
 
+    try {
+      await sendPasswordResetEmail(auth, forgotPasswordEmail);
+      alert("Un e-mail de réinitialisation a été envoyé à votre adresse e-mail.");
+      setForgotPasswordEmail("");
+      // Fermer le modal après la soumission réussie
+      setIsModalOpen(false);
+    } catch (error) {
+      alert("Échec de l'envoi de l'e-mail de réinitialisation. Veuillez réessayer.");
+      console.error(error);
+    }
   };
+
   return (
-    <div className="container">
+    <div className={`container ${isModalOpen ? 'd-block' : 'd-none'}`}>
       <div className="row justify-content-center">
         <div class="col-md-6 mt-5  first">
           <div class="card-body  p-3  text-center text-white">
             <h2 class="fw-bold text-dark mb-5">Mot de passe Oublié?</h2>
-            <form action="" onSubmit={(e) => handleSubmit(e)}>
+            <form onSubmit={handleSubmit}>
               {/* Email input */}
               <div class="form-outline text-start mb-4">
                 <input
@@ -27,14 +37,14 @@ export default function OubliMoPass() {
                   class="form-control p-2"
                   required
                   placeholder="Saisissez votre mail"
+                  value={forgotPasswordEmail}
+                  onChange={(e) => setForgotPasswordEmail(e.target.value)}
                 />
               </div>
-              {/* <!-- Password input --> */}
-
               {/* <!-- Submit button --> */}
               <div className="text-end justify-content-between">
                 <button type="button" class="btn btn-block  btn-secondary ">
-                  <Link to="/connecxion" className='text-decoration-none text-white'>Fermer</Link>
+                  <Link to="/connexion" className='text-decoration-none text-white'>Fermer</Link>
                 </button>
                 <button type="submit" className="btn but btn-block  text-white">
                   Envoyer
