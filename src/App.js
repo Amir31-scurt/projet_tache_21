04f1a272badebce515b364de1d25d13ad6a4d1d9
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2023-12-20 15:06:39 +0000

    WIP: Template Work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import SideBar from './components/CompoDashCoach/Sous_CompoSideBar/SideBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Template from './layout/template';
 import 'rsuite/dist/rsuite.min.css';
@@ -7,10 +6,7 @@ import 'rsuite/dist/rsuite.min.css';
 function App() {
   return (
     <div className="">
-      <SideBar />
-      {/*====== Le Template Debut======*/}
       <Template />
-      {/*====== Le Template Fin======*/}
     </div>
   );
 }
--- src/components/CompoDashCoach/Sous_CompoNavBar/NavBarCompo.jsx
+++ src/components/CompoDashCoach/Sous_CompoNavBar/NavBarCompo.jsx
@@ -1,22 +1,21 @@
-import React from "react";
-import "bootstrap/dist/js/bootstrap.bundle";
-import "bootstrap/dist/css/bootstrap.min.css"; // Importer le fichier CSS de Bootstrap
-import LogoTech from "../../../../src/assets/images/Logo.png";
-import UserProfil from "../../../../src/assets/images/user.png";
-import { MdMessage } from "react-icons/md";
-import { IoNotifications } from "react-icons/io5";
-import { Modal, ButtonToolbar, Button, Loader, Placeholder } from "rsuite";
+import React from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importer le fichier CSS de Bootstrap
+import LogoTech from '../../../assets/images/logo.png';
+import UserProfil from '../../../../src/assets/images/user.png';
+import { MdMessage } from 'react-icons/md';
+import { IoNotifications } from 'react-icons/io5';
+import { Modal, ButtonToolbar, Button, Loader, Placeholder } from 'rsuite';
 
 export const NavBarCompo = () => {
+  const [open, setOpen] = React.useState(false);
+  const [rows, setRows] = React.useState(0);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-   const [open, setOpen] = React.useState(false);
-   const [rows, setRows] = React.useState(0);
-   const handleOpen = () => setOpen(true);
-   const handleClose = () => setOpen(false);
-
-   const handleEntered = () => {
-     setTimeout(() => setRows(80), 2000);
-   };
+  const handleEntered = () => {
+    setTimeout(() => setRows(80), 2000);
+  };
 
   return (
     <div>
@@ -25,11 +24,7 @@ export const NavBarCompo = () => {
         <div className="NavBarContainer d-flex">
           <div className="LogoConta d-flex align-items-center justify-content-center">
             <div className="LogoConta2">
-              <img
-                src={LogoTech}
-                alt="Le Logo"
-                className="img-fluid LOGONAV"
-              />
+              <img src={LogoTech} alt="Le Logo" className="img-fluid LOGONAV" />
               <h3 className=" d-none d-lg-block text-white ms-2">TechGenius</h3>
             </div>
           </div>
@@ -71,7 +66,7 @@ export const NavBarCompo = () => {
                 {rows ? (
                   <Placeholder.Paragraph rows={rows} />
                 ) : (
-                  <div style={{ textAlign: "center" }}>
+                  <div style={{ textAlign: 'center' }}>
                     <Loader size="md" />
                   </div>
                 )}
