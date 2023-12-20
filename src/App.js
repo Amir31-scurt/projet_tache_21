0886c333a58ca0f8f506b805022351c5457a9dec
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2023-12-20 13:08:39 +0000

    Reusable cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Cards from './components/cards';
 
 function App() {
   return (
     <div className="App">
+      <Cards />
       <h1>Hello World</h1>
     </div>
   );
