6931aa515e74bbac4016b3b53ff1c760fbb0d6e9
Mourtallasyll <serignemourtallasyll86@gmail.com>
2023-12-23 23:20:25 +0000

    Integration du filtre pour un etudiant specifique

--- src/components/CompoDashCoach/CardLivraison.js
+++ src/components/CompoDashCoach/CardLivraison.js
@@ -4,21 +4,28 @@ import image from "../../assets/images/image1.png";
 import { Button, Modal } from "react-bootstrap";
 import { useState } from "react";
 import CarouselCard from "./CarouselCard";
-
+//Composant de Cartes contenus dans la partie Livraison du coach
 function CardLivraison() {
   const [show, setShow] = useState(false);
+  //Fonction pour l'ouverture du modal
   const handleClose = () => setShow(false);
+  //Fonction pour la fermeture du modal
   const handleShow = () => setShow(true);
   return (
-    <Card style={{ width: "18rem" }} className="cardLivraison">
-      <Card.Img variant="top" src={image} />
+    <Card style={{ width: "18rem" }} className="cardLivraison col-md-4 mx-3 my-3">
+      <Card.Img variant="top" src={image} className="imgCard"/>
       <Card.Body>
         <Card.Title>Tache 02 JavaScript</Card.Title>
-        <i
-          className="bi bi-eye-fill text-info fs-3"
-          onClick={handleShow}
-          role="button"
-        ></i>
+        <p>Serigne Mourtalla Syll</p>
+        <div className="d-flex justify-content-between">
+          <i
+            className="bi bi-eye-fill fs-3"
+            onClick={handleShow}
+            id="viewDetails"
+            role="button"
+          ></i>
+          <span className="d-flex align-items-center">Il y'a 1 jour</span>
+        </div>
       </Card.Body>
       <Modal show={show} onHide={handleClose} keyboard={false} className="modals">
         <Modal.Header closeButton>
