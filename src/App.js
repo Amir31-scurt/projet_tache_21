81927e5dac9a0dc79c9df6c14d90cda8851916a3
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2023-12-26 08:26:41 +0000

    Admin

--- src/App.js
+++ src/App.js
@@ -1,68 +1,14 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Template from './layout/template';
+import 'rsuite/dist/rsuite-no-reset.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'rsuite/dist/rsuite.min.css';
-import Connexion from './pages/Connexion';
-import DashboardApprenant from './pages/DashboardApprenant';
-import OubliMoPass from './pages/OubliMoPass';
-import ChatHome from './components/chatComponent/ChatHome';
-// import Programme from './components/Programme';
-import ProgrammeCoach from './components/programmes/programmes';
-import SpecificPro from './components/programmes/Single_Programmes/specific_program';
-import Certificate from './components/BulletinEtudiant';
-import AuthContextProvider from './contexte/AuthContext';
-import CardLivraison from './components/CompoDashCoach/CardLivraison';
-import ContentCardLivraison from './components/ContentCardLivraison';
+import Table from './components/super-admin/Table';
 
-export default function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Connexion />,
-    },
-    {
-      path: '/modal',
-      element: <OubliMoPass />,
-    },
-    {
-      path: '/dashboard',
-      element: <Template />,
-      children: [
-        {
-          index: true, // Index route for /timeline
-          element: <DashboardApprenant />,
-        },
-        {
-          path: '/dashboard/programme', // Relative path
-          element: <ProgrammeCoach />,
-        },
-        {
-          index: true,
-          path: '/dashboard/chatHome', // Relative path
-          element: <ChatHome />,
-        },
-        {
-          path: '/dashboard/programme/cours',
-          element: <SpecificPro />,
-        },
-        {
-          path: '/dashboard/certificat',
-          element: <Certificate />,
-        },
-        {
-          path: '/dashboard/livrable',
-          element: <ContentCardLivraison />,
-        },
-      ],
-    },
-  ]);
-
+function App() {
   return (
-    <AuthContextProvider>
-      <div className="">
-        <RouterProvider router={router} />
-      </div>
-    </AuthContextProvider>
+    <div className="App">
+      <Table />
+    </div>
   );
 }
+
+export default App;
