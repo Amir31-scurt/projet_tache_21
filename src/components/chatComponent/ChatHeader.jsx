ef7b715e1a85315451b51fb5d36259b5033a63ca
Sam Laye <layesamss97@gmail.com>
2023-12-28 19:10:26 +0000

    WIP: Chat_28-12-2023

--- src/components/chatComponent/ChatHeader.jsx
+++ src/components/chatComponent/ChatHeader.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import ChatNavbar from "./ChatNavbar";
 import Chats from "./Chats";
 import Search from "./Search";
+import { ChatAuthCtx } from "../../contexte/ChatAuthCtx";
 
 export default function ChatHeader() {
   const [openSearch, setOpenSearch] = useState(false);
+
+  const { currentUser } = useContext(ChatAuthCtx);
+  console.log(currentUser);
 
   return (
     <div className="chatHeader">
       {/* <ChatNavbar /> */}
       <div className="d-flex justify-content-between chatHeaderBtnContainer text-white p-1">
-        <span>currentUser.name</span>
+        <span>{currentUser && currentUser.displayName}</span>
         {openSearch ? (
           <i
             className="bi bi-x-lg my-auto me-3"
