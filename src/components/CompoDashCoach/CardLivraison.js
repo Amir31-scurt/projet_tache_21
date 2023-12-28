1f82d3f45e889f8daedfdba45a08111d44489823
diayaya1999@gmail.com <diayaya1999@gmail.com>
2023-12-28 13:11:22 +0000

    first commit

--- src/components/CompoDashCoach/CardLivraison.js
+++ src/components/CompoDashCoach/CardLivraison.js
@@ -12,42 +12,46 @@ function CardLivraison() {
   //Fonction pour la fermeture du modal
   const handleShow = () => setShow(true);
   return (
-      <Card
-        style={{ width: "20rem" }}
-        className="cardLivraison col-lg-4 col-md-6 my-3  mx-3 "
-      >
-        <Card.Img variant="top" src={image} className="imgCard" />
-        <Card.Body>
-          <Card.Title>Tache 02 JavaScript</Card.Title>
-          <p>Serigne Mourtalla Syll</p>
-          <div className="d-flex justify-content-between">
-            <i
-              className="bi bi-eye-fill fs-3"
-              onClick={handleShow}
-              id="viewDetails"
-              role="button"
-            ></i>
-            <span className="d-flex align-items-center">Il y'a 1 jour</span>
-          </div>
-        </Card.Body>
-        <Modal
-          show={show}
-          onHide={handleClose}
-          keyboard={false}
-          className="modals"
+    <div className="d-flex justify-content-center align-items-center">
+      <div className="d-flex flex-wrap justify-content-center">
+        <Card
+          style={{ width: "20rem" }}
+          className="cardLivraison  col-lg-4 col-md-6 my-3 column-gap-3 "
         >
-          <Modal.Header closeButton>
-            <Modal.Title>Livraisons</Modal.Title>
-          </Modal.Header>
-          <Modal.Body className="content-modal">
-            <CarouselCard />
-          </Modal.Body>
-          <Modal.Footer>
-            <Button className="bg-success border-0">Valider</Button>
-            <Button className="bg-danger border-0">Invalider</Button>
-          </Modal.Footer>
-        </Modal>
-      </Card>
+          <Card.Img variant="top" src={image} className="imgCard" />
+          <Card.Body>
+            <Card.Title>Tache 02 JavaScript</Card.Title>
+            <p>Serigne Mourtalla Syll</p>
+            <div className="d-flex justify-content-between">
+              <i
+                className="bi bi-eye-fill fs-3"
+                onClick={handleShow}
+                id="viewDetails"
+                role="button"
+              ></i>
+              <span className="d-flex align-items-center">Il y'a 1 jour</span>
+            </div>
+          </Card.Body>
+          <Modal
+            show={show}
+            onHide={handleClose}
+            keyboard={false}
+            className="modals"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Livraisons</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="content-modal">
+              <CarouselCard />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button className="bg-success border-0">Valider</Button>
+              <Button className="bg-danger border-0">Invalider</Button>
+            </Modal.Footer>
+          </Modal>
+        </Card>
+      </div>
+    </div>
   );
 }
 
