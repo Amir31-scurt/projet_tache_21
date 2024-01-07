c248982e62184beabf272e92066e8fb22f95d842
ndongo1203 <ondjrdemette1203@gmail.com>
2024-01-07 17:35:32 +0000

    Recuperation du nombres etudiants et coach

--- src/components/DashboardApprenant.jsx
+++ src/components/DashboardApprenant.jsx
@@ -1,17 +1,83 @@
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import CardLivraison from './CardLivraison';
 import DashboardCompo from './programmes/Single_Programmes/DashboardCompo';
-import { ContenuCardDsb } from './CompoDashCoach/Sous_CompoSideBar/Utils';
+import { Users } from './CompoDashCoach/Sous_CompoSideBar/Utils';
+import { MdTask } from 'react-icons/md';
+import { FaUsers } from 'react-icons/fa';
+import { PiUsersFourFill } from 'react-icons/pi';
+import {
+  collection,
+  onSnapshot,
+} from 'firebase/firestore';
+import { db } from '../config/firebase-config';
 
 export default function DashboardApprenant() {
+  // L'stockage des utilisateurs récupérés depuis Firestore
+  const [users, setUsers] = useState([]);
+
+  // Filtre des utilisateurs par rôle (Coach ou Étudiant)
+  const teachers = users.filter((user) => user.role === "Coach");
+  const students = users.filter((user) => user.role === "Étudiant");
+
+  // Fonction pour charger les utilisateurs depuis Firestore
+  const loadUsers = useCallback(() => {
+    // Initialisation du listener de snapshot pour la collection 'utilisateurs'
+    const unsubscribe = onSnapshot(collection(db, 'utilisateurs'), (snapshot) => {
+      // Mettre à jour le state avec les données utilisateur actualisées
+      const updatedUsers = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setUsers(updatedUsers);
+    });
+
+    // Nettoyage de listener lors du démontage du composant
+    return () => {
+      unsubscribe();
+    };
+  }, []); 
+
+  // Utilisation useEffect pour appeler loadUsers lors du montage du composant
+  useEffect(() => {
+    loadUsers();
+  }, [loadUsers]); 
+
+  // Données pour les cartes du dashboard
+  const ContenuCardDsb = [
+    {
+      ChiffreCardDsb: teachers.length,
+      IconeCardDsb: (
+        <FaUsers style={{ fontSize: '68px', opacity: '1', color: '#fff' }} />
+      ),
+      TextCardDsb: 'Professeurs',
+      couleurCarte: 'CouleurA',
+    },
+    {
+      ChiffreCardDsb: students.length,
+      IconeCardDsb: (
+        <PiUsersFourFill
+          style={{ fontSize: '68px', opacity: '1', color: '#fff' }}
+        />
+      ),
+      TextCardDsb: 'Etudiants',
+      couleurCarte: 'CouleurB',
+    },
+    {
+      ChiffreCardDsb: '52',
+      IconeCardDsb: (
+        <MdTask style={{ fontSize: '68px', opacity: '1', color: '#fff' }} />
+      ),
+      TextCardDsb: 'Taches',
+      couleurCarte: 'CouleurC',
+    },
+  ];
+
   return (
-    <div className="d-flex flex-column ms-3 justify-content-center">
-      <h1
-        className="fst-italic text-secondary fs-3 fw-bold ps-2 pt-3" 
-      >
+    <div className="d-flex flex-column flex-wrap ms-3 justify-content-center">
+      <h1 className="fst-italic text-secondary fs-3 fw-bold ps-2 pt-3">
         Dashboard
       </h1>
-      {/*========= Les cartes du dashboard Debut =========*/}
+      {/* Cartes du dashboard */}
       <div className="d-flex ContaCardDsb justify-content-start">
         {ContenuCardDsb.map((elem, index) => (
           <DashboardCompo {...elem} key={index} />
