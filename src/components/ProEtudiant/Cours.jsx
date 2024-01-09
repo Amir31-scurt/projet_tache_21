83247cf59b4f89c1e59bc88d35ffd0a92d5a7759
kessoba <mariamabahdiallo1@gmail.com>
2024-01-09 13:07:54 +0000

    Hello

--- src/components/ProEtudiant/Cours.jsx
+++ src/components/ProEtudiant/Cours.jsx
@@ -1,24 +1,68 @@
 import React, { useState, useEffect, useContext,  } from 'react';
 import { Card } from 'primereact/card';
 import { Modal } from 'rsuite';
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { db } from '../../config/firebase-config';
-import { getDoc, doc } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { format } from 'date-fns';
+import { addDoc, collection } from 'firebase/firestore';
+import { db, storage } from '../../config/firebase-config';
+import AuthContextProvider, { AuthContext } from '../../contexte/AuthContext';
+
 
 export default function Cours() {
-  const { domaineId, sousDomaineName } = useParams();
-  function CarteCours() {
-    const [courses, setCourses] = useState([]);
-    const [display, setDisplay] = useState(false);
-    const [changement, setChangement] = useState(false);
-    const [livraison, setLivraison] = useState(true);
-    const handleDisplay = () => {
-      setDisplay(true);
-      setChangement(true);
-      setLivraison(false);
+  const [display, setDisplay] = useState(false);
+  const [changement, setChangement] = useState(false);
+  const [livraison, setLivraison] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [previews, setPreviews] = useState([]);
+  const [backdrop, setBackdrop] = useState('static');
+
+  const {currentUser, uid} = useContext(AuthContext);
+  
+  const  UserEmail = currentUser.email;
+  const UserUid = uid ; 
+  console.log("Le uid =", UserUid);
+
+  const handleDisplay = () => {
+    setDisplay(true);
+    setChangement(true);
+    setLivraison(false);
+  };
+
+  const handleChangement = () => {
+    setChangement(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleFileChange = (e) => {
+    const { files } = e.target;
+    if (files && files.length > 0) {
+      const newFiles = Array.from(files);
+      setSelectedFiles((prevFiles) => [...prevFiles, ...newFiles]);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedFiles.length === 0) {
+      setPreviews([]);
+      return;
+    }
+
+    const objectUrls = selectedFiles.map((file) => URL.createObjectURL(file));
+    setPreviews(objectUrls);
+
+    // free memory
+    return () => {
+      objectUrls.forEach((url) => URL.revokeObjectURL(url));
     };
-  } [selectedFiles];
+  }, [selectedFiles]);
 
   const handleUpload = async (user) => {
     // Loop through selected files and upload each to Firebase Storage
@@ -36,7 +80,7 @@ export default function Cours() {
     setOpen(false);
 
     if (selectedFiles.length > 0 ) {
-      // Lo op through selected files and add each to Firestore
+      // Loop through selected files and add each to Firestore
       selectedFiles.forEach(async (file) => {
         const imageUrls = [];
         const storageRef = ref(storage, `Images/${UserUid}/${file.name}`);
@@ -54,185 +98,92 @@ export default function Cours() {
         });
       });
 
-    const getYouTubeVideoId = (url) => {
-      if (typeof url !== 'string') {
-        return null;
-      }
-      const regExp =
-        /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-      const match = url.match(regExp);
-      return match && match[2].length === 11 ? match[2] : null;
-    };
-
-    useEffect(() => {
-      const fetchCourses = async () => {
-        try {
-          const docRef = doc(db, 'domaines', domaineId);
-          const docSnap = await getDoc(docRef);
-
-          if (docSnap.exists()) {
-            const domaineData = docSnap.data();
-            const sousDomaine = domaineData.sousDomaines[sousDomaineName];
-            console.log(sousDomaine);
-
-            // Check if sousDomaine and sousDomaine.cours exist
-            if (sousDomaine && Array.isArray(sousDomaine.cours)) {
-              setCourses(sousDomaine.cours); // Assuming cours is already an array
-              console.log(sousDomaine.cours);
-
-              // Map over the cours array
-              courses.forEach((course) => {
-                console.log(`Title: ${course.title}`);
-                console.log(`Link: ${course.link}`);
-                console.log(`Description: ${course.description}`);
-              });
-            } else {
-              console.log('No courses found for the provided sous-domaine');
-            }
-          } else {
-            console.log('No such document!');
-          }
-        } catch (error) {
-          console.error('Error fetching courses: ', error);
-        }
-      };
-
-      console.log('Domaine ID:', domaineId);
-      console.log('Sous Domaine Name:', sousDomaineName);
-
-      fetchCourses();
-    }, [domaineId, sousDomaineName]);
-
-    return (
-      <div className="card" style={{ padding: '20px' }}>
-        {courses.map((course, index) => {
-          const videoId = getYouTubeVideoId(course);
-          const isYouTubeLink = videoId !== null;
-          const embedUrl = isYouTubeLink
-            ? `https://www.youtube.com/embed/${videoId}`
-            : null;
-
-          return (
-            <div key={index} className="col-12 col-md-6 col-lg-4">
-              <Card style={{ padding: '20px' }}>
-                <h4>{course.title}</h4>
-                <p>{course.description}</p>
-                {isYouTubeLink ? (
-                  <iframe
-                    src={embedUrl}
-                    title={`YouTube video player for ${course.title}`}
-                    allowFullScreen
-                    width="100%"
-                    height="250px"
-                  ></iframe>
-                ) : (
-                  <a href={course} target="_blank" rel="noopener noreferrer">
-                    {course}
-                  </a>
-                )}
-                <div className="d-flex justify-content-end">
-                  <button
-                    className={` btn text-white ${
-                      livraison ? 'd-none' : 'd-block'
-                    }`}
-                    onClick={handleOpen}
-                    style={{ backgroundColor: '#48a93c' }}
-                  >
-                    Livrer
-                  </button>
-                  <button
-                    className={` btn text-white ${
-                      changement ? 'd-none' : 'd-block'
-                    }`}
-                    onClick={handleDisplay}
-                    style={{ backgroundColor: '#48a93c' }}
-                  >
-                    Demarer
-                  </button>
-                  <button
-                    onClick={handleChangement}
-                    className={` btn text-white ${
-                      display ? 'd-block' : 'd-none'
-                    }`}
-                    style={{ backgroundColor: '#3084b5' }}
-                  >
-                    Terminer
-                  </button>
-                </div>
-              </Card>
-            </div>
-          );
-        })}
-        <div className="text-end mt-2 gap-2 d-flex justify-content-end ">
-          <Modal
-            backdrop={backdrop}
-            keyboard={false}
-            open={open}
-            onClose={handleClose}
-          >
-            <Modal.Header>
-              <Modal.Title>Envoyer mon travail</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <div className="mb-3">
-                <textarea
-                  placeholder="description"
-                  class="form-control"
-                  id="exampleFormControlTextarea1"
-                  rows="3"
-                ></textarea>
-              </div>
-              {previews &&
-                previews.map((pic) => {
-                  return (
-                    <div className="d-flex m-3 ">
-                      <img src={pic} className="output " />
-                    </div>
-                  );
-                })}
-              <div className="text-center mt-4">
-                <label
-                  for="formFileLg"
-                  id="myfiles"
-                  className="form-label inputStyle "
-                >
-                  Choisir Fichiers
-                </label>
-
-                <input
-                  className="form-control d-none "
-                  onChange={(e) => {
-                    if (e.target.files && e.target.files.length > 0) {
-                      setFiles(e.target.files);
-                    }
-                  }}
-                  multiple
-                  accept="image/*"
-                  id="formFileLg"
-                  type="file"
-                />
-              </div>
-            </Modal.Body>
-            <Modal.Footer>
-              <button type="submit" className="inputStyle">
-                Envoyer
-              </button>
-            </Modal.Footer>
-          </Modal>
-        </div>
-      </div>
-    );
-  }
+      // Clear selected files
+      setSelectedFiles([]);
+      // Close the modal
+      setOpen(false);
+    }
+    
+  };
 
   return (
-    <div className="bg-cours p-2">
-      <h2>Course List</h2>
-      <div className="container">
-        <div className="row">
-          <CarteCours domaineId={domaineId} sousDomaineName={sousDomaineName} />
-        </div>
+    <div className="card" style={{ padding: '20px' }}>
+      <Card style={{ border: '1px solid #48a93c', padding: '30px' }}>
+        <h5>html</h5>
+        <p>durée</p>
+        <p className="">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          sed consequuntur error repudiandae numquam deserunt quisquam
+          repellat libero asperiores earum nam nobis, culpa ratione quam
+          perferendis esse, cupiditate neque quas!
+        </p>
+      </Card>
+
+      <div className="text-end mt-2 gap-2 d-flex justify-content-end ">
+        <Modal backdrop={backdrop} keyboard={false} open={open} onClose={handleClose}>
+          <Modal.Header>
+            <Modal.Title>Envoyer mon travail</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="mb-3">
+              <textarea
+                placeholder="description"
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
+            </div>
+            {previews &&
+              previews.map((pic) => {
+                return (
+                  <div className="d-flex m-3 " key={pic}>
+                    <img src={pic} className="output " alt="Preview" />
+                  </div>
+                );
+              })}
+            <div className="text-center mt-4">
+              <label htmlFor="formFileLg" id="myfiles" className="form-label inputStyle ">
+                Choisir Fichiers
+              </label>
+
+              <input
+                className="form-control d-none "
+                onChange={handleFileChange}
+                multiple
+                accept="image/*"
+                id="formFileLg"
+                type="file"
+              />
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <button type="submit" onClick={handleUpload} className="inputStyle">
+              Envoyer
+            </button>
+          </Modal.Footer>
+        </Modal>
+        <button
+          className={` btn text-white ${livraison ? 'd-none' : 'd-block'}`}
+          onClick={handleOpen}
+          style={{ backgroundColor: '#48a93c' }}
+        >
+          Livrer
+        </button>
+        <button
+          className={` btn text-white ${changement ? 'd-none' : 'd-block'}`}
+          onClick={handleDisplay}
+          style={{ backgroundColor: '#48a93c' }}
+        >
+          Demarer
+        </button>
+        <button
+          onClick={handleChangement}
+          className={` btn text-white ${display ? 'd-block' : 'd-none'}`}
+          style={{ backgroundColor: '#3084b5' }}
+        >
+          Terminer
+        </button>
       </div>
     </div>
   );
-}
 }
--- src/utils/cards_reusable.jsx
+++ src/utils/cards_reusable.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import { collection, onSnapshot } from 'firebase/firestore';
+import React, {useCallback, useContext, useEffect, useState} from 'react';
+import { db } from '../config/firebase-config';
+import { EmailContext } from '../contexte/EmailContexte';
 
-export default function Card({ title, description, imageUrl, buttonText }) {
+export default function Card() {
+  const [users, setUsers] = useState([]);
+  const {email} = useContext(EmailContext)
+
+  useEffect(() => {
+    const usersStudentsUnsub = onSnapshot(
+      collection(db, 'utilisateurs'),
+      (snapshot) => {
+        const updatedStudents = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setUsers(updatedStudents);
+      }
+    );
+
+    return () => {
+      usersStudentsUnsub(); // Clean up the listener
+    };
+  }, []); // Removed roleUser from the dependency array
+
+  const roleUser = users.find((user) => user.email === email);
+  const userCoach = users.find((user) => user.email === roleUser.emailCoach)
+  console.log(roleUser, userCoach)
+
   return (
-    <div className="program-card2 card bg-light">
-      <img src={imageUrl} alt={title} />
-      <h3>Coach</h3>
-      <p>{description}ijjjoiiuuu</p>
-      <button>{buttonText}</button>
+    <div className="program-card2 card bg-light justify-content-center">
+      <div className='d-flex justify-content-between '>
+     <div className='nicePic'><img src={ userCoach? userCoach.photoURL:'' }/></div> 
+     <div>
+      <h4>{userCoach ? userCoach.name :''}</h4>
+      <h6>{userCoach ? userCoach.email :''}</h6>
+      </div>
+      </div>
     </div>
   );
 }
