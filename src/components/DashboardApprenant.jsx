d399ad11d27882a26a399e91355e74873713320c
ndongo1203 <ondjrdemette1203@gmail.com>
2024-01-09 17:53:35 +0000

    separation des cartes

--- src/components/DashboardApprenant.jsx
+++ src/components/DashboardApprenant.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, useState } from 'react';
+import React, { useEffect, useCallback, useState, useContext } from 'react';
 import CardLivraison from './CardLivraison';
 import DashboardCompo from './programmes/Single_Programmes/DashboardCompo';
 import { Users } from './CompoDashCoach/Sous_CompoSideBar/Utils';
@@ -10,15 +10,16 @@ import {
   onSnapshot,
 } from 'firebase/firestore';
 import { db } from '../config/firebase-config';
-
+import {EmailContext} from '../contexte/EmailContexte'
 export default function DashboardApprenant() {
   // L'stockage des utilisateurs récupérés depuis Firestore
   const [users, setUsers] = useState([]);
+  const {email} = useContext(EmailContext)
 
-  // Filtre des utilisateurs par rôle (Coach, Etudiant et cours)
+  // Filtre des utilisateurs par rôle (Coach ou Étudiant)
   const teachers = users.filter((user) => user.role === "Coach");
   const students = users.filter((user) => user.role === "Étudiant");
-  const cours = users.filter((user) => user.description === "Cours");
+  const userRole = users.find((user) => user.email === email);
 
   // Fonction pour charger les utilisateurs depuis Firestore
   const loadUsers = useCallback(() => {
@@ -36,14 +37,68 @@ export default function DashboardApprenant() {
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, []); 
 
   // Utilisation useEffect pour appeler loadUsers lors du montage du composant
   useEffect(() => {
     loadUsers();
-  }, [loadUsers]);
+  }, [loadUsers]); 
 
   // Données pour les cartes du dashboard
+
+  const contentCard = () =>{
+    const ContenuCardDsb = [];
+    if(userRole && userRole.role === "Coach"){
+      ContenuCardDsb.push({
+        ChiffreCardDsb: teachers.length,
+        IconeCardDsb: (
+          <FaUsers style={{ fontSize: '68px', opacity: '1', color: '#fff' }} />
+        ),
+        TextCardDsb: 'Professeurs',
+        couleurCarte: 'CouleurA',
+      },
+      {
+        ChiffreCardDsb: students.length,
+        IconeCardDsb: (
+          <PiUsersFourFill
+            style={{ fontSize: '68px', opacity: '1', color: '#fff' }}
+          />
+        ),
+        TextCardDsb: 'Etudiants',
+        couleurCarte: 'CouleurB',
+      },)
+    }else{
+      ContenuCardDsb.push(
+        {
+        ChiffreCardDsb: teachers.length,
+        IconeCardDsb: (
+          <FaUsers style={{ fontSize: '68px', opacity: '1', color: '#fff' }} />
+        ),
+        TextCardDsb: 'Professeurs',
+        couleurCarte: 'CouleurA',
+      },
+      {
+        ChiffreCardDsb: students.length,
+        IconeCardDsb: (
+          <PiUsersFourFill
+            style={{ fontSize: '68px', opacity: '1', color: '#fff' }}
+          />
+        ),
+        TextCardDsb: 'Etudiants',
+        couleurCarte: 'CouleurB',
+      },
+      {
+        ChiffreCardDsb: '52',
+        IconeCardDsb: (
+          <MdTask style={{ fontSize: '68px', opacity: '1', color: '#fff' }} />
+        ),
+        TextCardDsb: 'Taches',
+        couleurCarte: 'CouleurC',
+      },)
+    }
+
+    return ContenuCardDsb;
+  }
   const ContenuCardDsb = [
     {
       ChiffreCardDsb: teachers.length,
@@ -57,22 +112,21 @@ export default function DashboardApprenant() {
       ChiffreCardDsb: students.length,
       IconeCardDsb: (
         <PiUsersFourFill
-          style={{ fontSize: '68px', opacity: 1, color: '#fff' }}
+          style={{ fontSize: '68px', opacity: '1', color: '#fff' }}
         />
       ),
       TextCardDsb: 'Etudiants',
       couleurCarte: 'CouleurB',
     },
     {
-      ChiffreCardDsb: cours.length,
+      ChiffreCardDsb: '52',
       IconeCardDsb: (
         <MdTask style={{ fontSize: '68px', opacity: '1', color: '#fff' }} />
       ),
-      TextCardDsb: 'Cours',
+      TextCardDsb: 'Taches',
       couleurCarte: 'CouleurC',
     },
   ];
-
   return (
     <div className="d-flex flex-column flex-wrap ms-3 justify-content-center">
       <h1 className="fst-italic text-secondary fs-3 fw-bold ps-2 pt-3">
@@ -80,7 +134,7 @@ export default function DashboardApprenant() {
       </h1>
       {/* Cartes du dashboard */}
       <div className="d-flex ContaCardDsb justify-content-start">
-        {ContenuCardDsb.map((elem, index) => (
+        {contentCard().map((elem, index) => (
           <DashboardCompo {...elem} key={index} />
         ))}
       </div>
