2d9744b6f42ab11c3814d80d9a15a2f2b0754c7c
Mourtallasyll <serignemourtallasyll86@gmail.com>
2024-01-09 14:35:44 +0000

    Intégration de la logique sur les livraisons des étudiants

--- src/components/CompoDashCoach/CardLivraison.js
+++ src/components/CompoDashCoach/CardLivraison.js
@@ -1,12 +1,13 @@
 // import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import image from '../../assets/images/image1.png';
+// import image from '../../assets/images/image1.png';
 import { Button, Modal } from 'react-bootstrap';
 import { useState } from 'react';
-import CarouselCard from './CarouselCard';
+
 import * as Icon from "react-bootstrap-icons";
+import { Carousel } from 'rsuite';
 //Composant de Cartes contenus dans la partie Livraison du coach
-function CardLivraison({role}) {
+function CardLivraison({role, title, name, date, defaultImg, images, validation}) {
   const [show, setShow] = useState(false);
   //Fonction pour l'ouverture du modal
   const handleClose = () => setShow(false);
@@ -17,10 +18,10 @@ function CardLivraison({role}) {
       style={{ width: '21rem' }}
       className="cardLivraison flex-wrap bg-white shadow-4 rounded-4 mx-3 my-3"
     >
-      <Card.Img variant="top" src={image} className="imgCard rounded-3" />
+      <Card.Img variant="top" src={defaultImg} className="imgCard rounded-3" />
       <Card.Body>
-        <Card.Title>Tache 02 JavaScript</Card.Title>
-        <p>Serigne Mourtalla Syll</p>
+        <Card.Title>{title}</Card.Title>
+        <p>{name}</p>
         <div className="d-flex justify-content-between">
           <i
             className="bi bi-eye-fill fs-3"
@@ -28,7 +29,7 @@ function CardLivraison({role}) {
             role="button"
             style={{color: "#3084b5"}}
           ></i>
-          <span className="d-flex align-items-center">Il y'a 1 jour</span>
+          <span className="d-flex align-items-center">{"Il y'a 1 jour"}</span>
         </div>
       </Card.Body>
       <Modal
@@ -41,11 +42,18 @@ function CardLivraison({role}) {
           <Modal.Title>Livraisons</Modal.Title>
         </Modal.Header>
         <Modal.Body className="content-modal">
-          <CarouselCard />
+          <Carousel>
+            {images.map((img) => (
+              <img src={img} alt='img'/>
+            ))}
+          </Carousel>
         </Modal.Body>
         {role && role.role === 'Coach'? 
         <Modal.Footer>
-          <Button className="border-0 d-flex justify-content-center bg-warning pe-auto" disabled><Icon.PauseBtn className='fs-4 me-1' /> En cours...</Button>
+          {!validation ?
+            <Button className="border-0 d-flex justify-content-center bg-warning pe-auto" disabled><Icon.PauseBtn className='fs-4 me-1' /> En cours...</Button>:
+            <Button className="border-0 d-flex justify-content-center bg-success">Valider</Button>
+          }
           <Button className="bg-danger border-0">Rejeter</Button>
         </Modal.Footer> : ""}
       </Modal>
