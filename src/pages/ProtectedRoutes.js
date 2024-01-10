f21c37357b19630292952a6c3f5abecfb1988f58
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2024-01-10 20:01:30 +0000

    upgrade

--- src/pages/ProtectedRoutes.js
+++ src/pages/ProtectedRoutes.js
@@ -149,7 +149,7 @@ export default function ProtectedRoutes() {
           element: <ContentCardLivraison />,
         },
         {
-          path: 'etudiant/cours/:domaineId/:sousDomaineName',
+          path: 'etudiant/programme-apprenant/cours/:domaineId/:sousDomaineName',
           element: <Cours />,
         },
         {
